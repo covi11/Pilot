75476e7365c4af13e6865e4c0c8ecedc6790015d
Covenant <covi11dethemba@gmail.com>
2024-10-31 12:36:06 +0200

    Prrrr...

--- app.routes.ts
+++ app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './components/pages/landing/landing.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { LoginComponent } from './components/auth/login/login.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    {path: 'landing', component: LandingComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'contact', component: ContactComponent}
+
+];
